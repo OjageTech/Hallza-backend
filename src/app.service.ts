696e5a89c73e074a597f6ebff41de05d3ba026ff
Ojage <hismajestylordkratos@gmail.com>
2023-09-09 15:47:18 +0100

    by default, the start command in your package.json file will be nest start, make sure to update it to node dist/main, so that it serves the compiled main.js file from the dist folder. This dist folder will be created automatically after Cyclic starts to build your app with the nest build command

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World!';
+    return 'Hello World, this is the result of a get request on the root';
   }
 }
