e76b77cd89b7eab82c9302398b14394db892fd84
Ojage <hismajestylordkratos@gmail.com>
2023-09-10 05:57:11 +0100

    not start listening to PORT until the DB connection is established. Requests will be held in the Cyclic runtime to make sure none are ever missed, even if the connection takes a moment.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,16 +1,11 @@
 import { Module } from '@nestjs/common';
-import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { VenuesModule } from './venues/venues.module';
 import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [
-    MongooseModule.forRoot(process.env.MONGO_DB_URI),
-    VenuesModule,
-    AuthModule,
-  ],
+  imports: [VenuesModule, AuthModule],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/main.ts
+++ src/main.ts
@@ -2,16 +2,32 @@ import { NestFactory } from '@nestjs/core';
 import { config } from 'dotenv';
 import { AppModule } from './app.module';
 
+import { MongoClient } from 'mongodb';
+
 config();
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const uri = process.env.MONGO_DB_URI;
+  const PORT = process.env.PORT || 3000;
 
   app.enableCors({
-    allowedHeaders: ['content-type'],
+    allowedHeaders: ['Content-Type'],
     origin: '*',
     credentials: true,
   });
-  await app.listen(3000);
+
+  const client = new MongoClient(uri);
+
+  try {
+    await client.connect();
+    console.log('Connected to MongoDB');
+
+    await app.listen(PORT);
+    console.log(`Server is running on port ${PORT}`);
+  } catch (err) {
+    console.error('Error:', err);
+  }
 }
+
 bootstrap();
