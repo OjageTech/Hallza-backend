5ba3a94f92122c1b631288008fed9b69590b9e2c
Ojage <hismajestylordkratos@gmail.com>
2023-09-10 06:01:54 +0100

    undo

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,18 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { VenuesModule } from './venues/venues.module';
 import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [VenuesModule, AuthModule],
+  imports: [
+    MongooseModule.forRoot(
+      'mongodb+srv://salathielojage:g9WJiRkoyPQY7w8u@initialcluster.eu2cnk6.mongodb.net/?retryWrites=true&w=majority',
+    ),
+    VenuesModule,
+    AuthModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/main.ts
+++ src/main.ts
@@ -2,32 +2,16 @@ import { NestFactory } from '@nestjs/core';
 import { config } from 'dotenv';
 import { AppModule } from './app.module';
 
-import { MongoClient } from 'mongodb';
-
 config();
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  const uri = process.env.MONGO_DB_URI;
-  const PORT = process.env.PORT || 3000;
 
   app.enableCors({
-    allowedHeaders: ['Content-Type'],
+    allowedHeaders: ['content-type'],
     origin: '*',
     credentials: true,
   });
-
-  const client = new MongoClient(uri);
-
-  try {
-    await client.connect();
-    console.log('Connected to MongoDB');
-
-    await app.listen(PORT);
-    console.log(`Server is running on port ${PORT}`);
-  } catch (err) {
-    console.error('Error:', err);
-  }
+  await app.listen(3000);
 }
-
 bootstrap();
