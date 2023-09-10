a2a4385fdfb005b9fe440c44bb339d75f393125e
Ojage <hismajestylordkratos@gmail.com>
2023-09-10 21:23:35 +0100

    Started working on Signup auth

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,20 +1,20 @@
 import { Module } from '@nestjs/common';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
-// import { MongooseModule } from '@nestjs/mongoose';
-// import { PassportModule } from '@nestjs/passport';
-// import { UserSchema } from 'src/modules/auth/schemas/user.schema';
-// import { JwtModule } from '@nestjs/jwt';
+import { MongooseModule } from '@nestjs/mongoose';
+import { PassportModule } from '@nestjs/passport';
+import { UserSchema } from 'src/modules/auth/schemas/user.schema';
+import { JwtModule } from '@nestjs/jwt';
 
 @Module({
-  // imports: [
-  //   PassportModule.register({ defaultStrategy: 'jwt' }),
-  //   JwtModule.register({
-  //     secret: 'hallza_jwt', // Change this to your actual secret key
-  //     signOptions: { expiresIn: '3d' },
-  //   }),
-  //   MongooseModule.forFeature([{ name: 'User', schema: UserSchema }]),
-  // ],
+  imports: [
+    PassportModule.register({ defaultStrategy: 'jwt' }),
+    JwtModule.register({
+      secret: 'hallza_jwt', // Change this to your actual secret key
+      signOptions: { expiresIn: '3d' },
+    }),
+    MongooseModule.forFeature([{ name: 'User', schema: UserSchema }]),
+  ],
   controllers: [AuthController],
   providers: [AuthService],
 })
